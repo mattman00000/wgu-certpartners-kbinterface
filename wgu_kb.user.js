e5adb5e009c08de507e60a4da58a6dcbbdea0f79
mattman00000 <mattman00000@gmail.com>
2015-09-29 19:39:06 -0700

    added update url

--- wgu_kb.user.js
+++ wgu_kb.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         WGU education2 certification partners test keyboard interface
 // @namespace    http://www.mattman00000.com
-// @version      0.0.2
+// @version      0.0.3
 // @description  adds keyboard interface for practice tests e.g. C479 web technologies "course mastery"
 // @author       mattman00000
 // @match        https://education2.certification-partners.com/TestTake/testEntryPoint/*
 // @grant        none
+// @updateURL    https://github.com/mattman00000/wgu-certpartners-kbinterface/raw/master/wgu_kb.user.js
 // ==/UserScript==
 
 window.onkeyup = function(e) {
