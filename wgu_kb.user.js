d09a2737a7f2e4cf8c3034a5445dbe88222b64bf
mattman00000 <mattman00000@gmail.com>
2015-10-05 19:27:24 -0700

    implemented academic failure handling

--- wgu_kb.user.js
+++ wgu_kb.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         WGU education2 certification partners test keyboard interface
 // @namespace    http://www.mattman00000.com
-// @version      0.1.0
+// @version      0.1.1
 // @description  adds keyboard interface for practice tests e.g. C479 web technologies "course mastery"
 // @author       mattman00000
-// @match        https://education2.certification-partners.com/TestTake/testEntryPoint/*
+// @match        https://education2.certification-partners.com/*
 // @grant        none
 // @updateURL    https://github.com/mattman00000/wgu-certpartners-kbinterface/raw/master/wgu_kb.user.js
 // ==/UserScript==
 
+if (window.location.href.substring(0,window.location.href.lastIndexOf("/"))=="https://education2.certification-partners.com/TestTake/testEntryPoint") {
 var cycler = 0;
-var place = window.location.href;
+//var place = window.location.href;
 
 window.onkeyup = function(e) {
     var key = e.keyCode ? e.keyCode : e.which;
@@ -26,8 +27,10 @@ window.onkeyup = function(e) {
         if (document.getElementById("nextQuestionButton")!=null) {document.getElementById("nextQuestionButton").click();} else
         if (document.getElementById("nextSection")!=null) {document.getElementById("nextSection").click();} else
         if (document.getElementById("te-FinalGrade")!=null) {
-            if (document.getElementById("te-FinalGrade").innerText=="Further Study Required")
-                {document.getElementsByClassName("te-exit")[0].click(); window.setTimeout(function() {window.location.assign(place);},300);} else
+            if (document.getElementById("te-FinalGrade").innerText=="Further Study Required") {
+                localStorage.wguhref = window.location.href;
+                localStorage.wgufail = "true";
+            }
         if (document.getElementsByClassName("te-exit")[0]!=null) {document.getElementsByClassName("te-exit")[0].click();}} else
         { console.error("something weird happened"); }
     }else if (key == 82) {
@@ -62,4 +65,8 @@ window.onkeyup = function(e) {
             }
         }
     }
+}
+} else if ((window.location.href=="https://education2.certification-partners.com/view/desktop/home.asp")&&(localStorage.wgufail=="true")) {
+    localStorage.wgufail = "false";
+    window.location.href = localStorage.wguhref;
 }
