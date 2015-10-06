3a038904c9c42a8d3e7158ec50b2f11171b8215a
mattman00000 <mattman00000@gmail.com>
2015-10-05 17:38:10 -0700

    implemented result view cycling

--- wgu_kb.user.js
+++ wgu_kb.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         WGU education2 certification partners test keyboard interface
 // @namespace    http://www.mattman00000.com
-// @version      0.0.7
+// @version      0.0.8
 // @description  adds keyboard interface for practice tests e.g. C479 web technologies "course mastery"
 // @author       mattman00000
 // @match        https://education2.certification-partners.com/TestTake/testEntryPoint/*
 // @grant        none
 // @updateURL    https://github.com/mattman00000/wgu-certpartners-kbinterface/raw/master/wgu_kb.user.js
 // ==/UserScript==
+
+var cycler = 0;
 
 window.onkeyup = function(e) {
     var key = e.keyCode ? e.keyCode : e.which;
@@ -24,5 +26,34 @@ window.onkeyup = function(e) {
         document.getElementById("nextSection")!=null?document.getElementById("nextSection").click():
         document.getElementsByClassName("te-exit")[0]!=null?document.getElementsByClassName("te-exit")[0].click():
         console.error("something weird happened");
+    }else if (key == 82) {
+        if (cycler == 0) {
+            document.getElementById("question_responses").style.display = "block";
+            for (var i = 0; i<document.getElementsByClassName("correctContainer").length; i++) {
+                document.getElementsByClassName("correctContainer")[i].style.display = "none";
+            }
+            for (var i = 0; i<document.getElementsByClassName("incorrectContainer").length; i++) {
+                document.getElementsByClassName("incorrectContainer")[i].style.display = "block";
+            }
+            cycler++;
+        } else if (cycler==1) {
+            document.getElementById("question_responses").style.display = "block";
+            for (var i = 0; i<document.getElementsByClassName("correctContainer").length; i++) {
+                document.getElementsByClassName("correctContainer")[i].style.display = "block";
+            }
+            for (var i = 0; i<document.getElementsByClassName("incorrectContainer").length; i++) {
+                document.getElementsByClassName("incorrectContainer")[i].style.display = "block";
+            }
+            cycler++;
+        } else {
+            document.getElementById("question_responses").style.display = "none";
+            for (var i = 0; i<document.getElementsByClassName("correctContainer").length; i++) {
+                document.getElementsByClassName("correctContainer")[i].style.display = "none";
+            }
+            for (var i = 0; i<document.getElementsByClassName("incorrectContainer").length; i++) {
+                document.getElementsByClassName("incorrectContainer")[i].style.display = "none";
+            }
+            cycler=0;
+        }
     }
 }
