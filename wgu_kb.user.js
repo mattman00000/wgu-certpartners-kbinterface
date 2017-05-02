2f8e3064fd7a01dedc8687b798d74c42bcbd6de8
mattman00000 <mattman00000@gmail.com>
2017-05-02 15:30:14 -0700

    0.1.9.0 header and formatting

--- wgu_kb.user.js
+++ wgu_kb.user.js
@@ -1,20 +1,28 @@
 // ==UserScript==
-// @name         WGU education2 certification partners test keyboard interface
-// @namespace    http://www.mattman00000.com
-// @version      0.1.8
-// @description  adds keyboard interface for practice tests e.g. C479 web technologies "course mastery"
-// @author       mattman00000
-// @match        https://education2.certification-partners.com/*
-// @grant        none
-// @updateURL    https://github.com/mattman00000/wgu-certpartners-kbinterface/raw/master/wgu_kb.user.js
-// @downloadURL  https://github.com/mattman00000/wgu-certpartners-kbinterface/raw/master/wgu_kb.user.js
+// @name               WGU education2 certification partners test keyboard interface
+// @namespace          http://www.mattman00000.com
+// @version            0.1.9.0
+// @description        adds keyboard interface for practice tests e.g. C479 web technologies "course mastery"
+// @author             mattman00000
+// @match              https://education2.certification-partners.com/*
+// @grant              none
+// @run-at             document-end
+// @connect            github.com/mattman00000
+// @connect            self
+// @connect            *
+// @downloadURL        https://github.com/mattman00000/wgu-certpartners-kbinterface/raw/master/flfusker_helper.user.js
+// @updateURL          https://github.com/mattman00000/wgu-certpartners-kbinterface/raw/master/flfusker_helper.user.js
+// @supportURL         https://github.com/mattman00000/wgu-certpartners-kbinterface/issues
+// @homepageURL        https://github.com/mattman00000/userscripts
+// @contributionURL    https://github.com/mattman00000/userscripts
+// @namespace          https://github.com/mattman00000
 // ==/UserScript==
 console.warn("WGU Keyboard Interface Initializing");
 console.info("localStorage.wguhref is "+localStorage.wguhref);
 console.info("localStorage.wgufail is "+localStorage.wgufail);
 
 var ayylmao = window.setInterval(function()
-{ if ((document.getElementsByClassName("te-nextunclickable").length!=0)||(document.getElementsByClassName("te-startunclickable").length!=0))
+{ if ((document.getElementsByClassName("te-nextunclickable").length!==0)||(document.getElementsByClassName("te-startunclickable").length!==0))
 {document.body.style.backgroundColor = "#ff8888";}
 else {document.body.style.backgroundColor = "#ffffff";} }, 20);
 
@@ -43,9 +51,9 @@ if (window.location.href.substring(0,window.location.href.lastIndexOf("/"))==
         }else if (key == 32) {
             console.debug("Space");
             cycler = 0;
-            if (document.getElementById("nextQuestionButton")!=null) {document.getElementById("nextQuestionButton").click();console.debug("Next Question");} else
-            if (document.getElementById("nextSection")!=null) {document.getElementById("nextSection").click(); console.debug("Next Section");} else
-            if (document.getElementById("te-FinalGrade")!=null) {
+            if (document.getElementById("nextQuestionButton")!==null) {document.getElementById("nextQuestionButton").click();console.debug("Next Question");} else
+            if (document.getElementById("nextSection")!==null) {document.getElementById("nextSection").click(); console.debug("Next Section");} else
+            if (document.getElementById("te-FinalGrade")!==null) {
                 if (document.getElementById("te-FinalGrade").innerText=="Further Study Required") {
                     console.warn("Test Failure Condition");
                     localStorage.wguhref = window.location.href;
@@ -60,13 +68,13 @@ if (window.location.href.substring(0,window.location.href.lastIndexOf("/"))==
                     console.info("localStorage.wgufail is "+localStorage.wgufail);
                 }
             console.debug("Exiting Test");
-            if (document.getElementsByClassName("te-exit")[0]!=null) {document.getElementsByClassName("te-exit")[0].click();}} else
+            if (document.getElementsByClassName("te-exit")[0]!==null) {document.getElementsByClassName("te-exit")[0].click();}} else
             { console.error("something weird happened"); }
         }else if (key == 82) {
             console.debug("R key press");
-            if (document.getElementById("question_responses")!=null) {
+            if (document.getElementById("question_responses")!==null) {
                 console.info("cycler value is "+cycler);
-                if (cycler == 0) {
+                if (cycler === 0) {
                     document.getElementById("question_responses").style.display = "block";
                     for (var i = 0; i<document.getElementsByClassName("correctContainer").length; i++) {
                         document.getElementsByClassName("correctContainer")[i].style.display = "none";
@@ -75,7 +83,7 @@ if (window.location.href.substring(0,window.location.href.lastIndexOf("/"))==
                         document.getElementsByClassName("incorrectContainer")[i].style.display = "block";
                     }
                     cycler++;
-                } else if (cycler==1) {
+                } else if (cycler === 1) {
                     document.getElementById("question_responses").style.display = "block";
                     for (var i = 0; i<document.getElementsByClassName("correctContainer").length; i++) {
                         document.getElementsByClassName("correctContainer")[i].style.display = "block";
@@ -96,9 +104,9 @@ if (window.location.href.substring(0,window.location.href.lastIndexOf("/"))==
                 }
             }
         }
-    }
+    };
 } else if ((window.location.href=="https://education2.certification-partners.com/view/desktop/home.asp")&&(localStorage.wgufail=="true")) {
-    console.debug("Fail Page Loaded")
+    console.debug("Fail Page Loaded");
     localStorage.wgufail = "false";
     console.info("localStorage.wguhref is "+localStorage.wguhref);
     console.info("localStorage.wgufail is "+localStorage.wgufail);
